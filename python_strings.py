15be3b166a6e74741fea8bfdaa5d4633a7c8559e
MohammadTheDev <mohammad.arab.dev@gmail.com>
2025-11-14 17:33:22 +0330

    practice on str in python

---
+++ python_strings.py
@@ -0,0 +1,25 @@
+# name = "Harry potter"
+# # print(name.title())
+# print(name.upper())
+# print(name.lower())
+
+# first_name = "harry"
+# last_name= "potter"
+# full_name = f"{first_name} {last_name}"
+# message = f"Hello, {full_name.title()}!"
+# print(message)
+
+# print("Python")
+# print("\tPython")
+# print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+# favorite_language = ' python '
+# print(favorite_language.rstrip())
+# print(favorite_language.lstrip())
+
+# kodamooz_url = 'https://kodamooz.com'
+# print(kodamooz_url.removeprefix('https://'))
+
+# message = "Hello World!"
+# # print(message.find("world"))
+# print(message.index("world"))
