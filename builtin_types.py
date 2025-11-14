8aa21ee287b7f2bf375a57df2bff714545772a40
MohammadTheDev <mohammad.arab.dev@gmail.com>
2025-11-14 12:21:36 +0330

    add 2 file for test and learn python

---
+++ builtin_types.py
@@ -0,0 +1,5 @@
+s = '''This is 
+a multiline
+string'''
+
+print(s)
---
+++ hello_world.py
@@ -0,0 +1,10 @@
+message = "Hello Python World!"
+# print(message)
+
+message = "Hello Python!"
+# print(message)
+
+universe_age = 14_000_000_000
+print(universe_age)
+
+SPEED_OF_THE_LIGHT = 299_792_458
