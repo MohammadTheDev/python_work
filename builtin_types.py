0604815e371461355c9bec06de2fe821509e0c8b
MohammadTheDev <mohammad.arab.dev@gmail.com>
2025-11-14 12:42:35 +0330

    add file for Practice types in python

--- builtin_types.py
+++ builtin_types.py
@@ -1,5 +1,25 @@
-s = '''This is 
-a multiline
-string'''
+# s = '''This is 
+# a multiline
+# string'''
 
-print(s)
+# print(s)
+
+# prime_numbers = [2,3,5,7]
+# s =  prime_numbers[0]
+# print(s)
+
+# tuple 
+# prime_numbers = (2, 3, 5, 7)
+# s= prime_numbers[0]
+# f = prime_numbers[2] = 44
+# print(f)
+
+# type range
+# r = range(1, 7)
+# f =  r[4]
+# print(f)
+
+# type dict
+# d = {'name': 'lale', 'age': 4}
+# print(d['name'])
+# print(d['age'])
