8acfafafc97b5dbf991a93236c7944481d4ca70d
MohammadTheDev <mohammad.arab.dev@gmail.com>
2025-11-17 23:46:44 +0330

    practice lists in python

---
+++ python_lists.py
@@ -0,0 +1,83 @@
+# countries = ['england', 'spain', 'italy', 'germany']
+# print(countries)
+
+# countries = ['england', 'spain', 'italy', 'germany']
+# print(countries[-1])
+
+# countries = ['england', 'spain', 'italy', 'germany']
+# message = f"David Beckham is from {countries[0].title()}"
+# print(message)
+
+# countries = ['Germany', 'England', 'Spain', 'Italy']
+# print(countries)
+
+# countries[2] = 'France'
+# print(countries)
+
+# countries = ['Germany', 'England', 'Spain', 'Italy']
+# print(countries)
+
+# countries.append('Japan')
+# print(countries)
+
+# countries = []
+# countries.append('England')
+# countries.append('Germany')
+# countries.append('Spain')
+
+# print(countries)
+
+# countries = ["England", "Spain", "Japan"]
+# print(countries)
+
+# countries.insert(1, 'France')
+# print(countries)
+
+# countries = ['England', 'Spain', 'Germany', 'France']
+# print(countries)
+
+# del countries[2]
+# print(countries)
+
+# brands = ['Acer', 'Dell', 'Asus', 'Sony']
+# print(brands)
+
+# first_owned = brands.pop(0)
+# last_owned = brands.pop()
+
+# print(f"The first laptop I owned was a {first_owned}")
+# print(f"The last laptop I owned was a {last_owned}")
+# print(brands)
+
+# countriees = ['England', 'Spain', 'Germany', 'France']
+# print(countriees)
+
+# countriees.remove('Spain')
+# print(countriees)
+
+# brands = ['Acer', 'Dell', 'Ass', 'Sony', 'Apple']
+# print(brands)
+
+# top_expensive = 'Apple'
+# brands.remove(top_expensive)
+# print(brands)
+# print(f"{top_expensive} is too expensive for me")
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.sort()
+# print(cars)
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# print("Here is the original list:")
+# print(cars)
+# print("\nHere is the sorted list:")
+# print(sorted(cars))
+# print("\nHere is the original list again:")
+# print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+print(len(cars))
