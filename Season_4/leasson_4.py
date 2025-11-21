928ff3156796b852c747f86445bd14c322c37fa6
mohammad <mohammad.arab.dev@gmail.com>
2025-11-21 22:01:18 +0330

    learn and practice slice in lists

--- Season_4/leasson_4.py
+++ Season_4/leasson_4.py
@@ -5,7 +5,52 @@
 # print(numbers)
 # print(sub_list)
 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-left_sub_list = numbers[:5]
-right_sub_list = numbers[5:]
-print
+# numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# left_sub_list = numbers[:5]
+# right_sub_list = numbers[5:]
+# print(left_sub_list)
+# print(right_sub_list)
+
+# numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# sub_list = numbers[-6:-3]
+# print(sub_list)
+
+# sub_list = numbers[:-3]
+# print(sub_list)
+
+# sub_list = numbers[-6:]
+# print(sub_list)
+
+# numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# sub_list = numbers[6:2:-1]
+# print(sub_list)
+
+# text = "Hello, World!"
+# substring = text[7:12]
+# print(substring)
+
+# cities = ['shiraz', 'tehran', 'isfahan', 'tabriz', 'ahwaz']
+# for city in cities[:3]:
+#     print(city.title())
+
+# x = 5
+# y = x
+# print(f"x = { x }")
+# print(f"y = { y }")
+
+# my_sports = ['football', 'chess', 'basketball', 'volleyball']
+# friend_sports = my_sports
+# print(my_sports)
+# print(friend_sports)
+
+# friend_sports.append('tennis')
+# print(my_sports)
+
+my_sports = ['football', 'chess', 'basketball', 'volleyball'] 
+friend_sports = my_sports[:]
+print(my_sports)
+print(friend_sports)
+
+friend_sports.append('tennis')
+print(my_sports)
+print(friend_sports)
