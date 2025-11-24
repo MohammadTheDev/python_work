82a53a6e0b9e731e855321917d40144784cc9d23
mohammad <mohammad.arab.dev@gmail.com>
2025-11-24 12:29:25 +0330

    finish the seasson 4 and work with list, tuple and slice

---
+++ Season_4/leasson_5.py
@@ -0,0 +1,20 @@
+# dimensions = (200, 50)
+# print(dimensions[0])
+# print(dimensions[1])
+
+# dimensions = (200, 50)
+# dimensions[0] = 250
+
+# dimensions = (200, 50)
+# print(dimensions)
+
+# dimensions = (400, 100)
+# print(dimensions)
+
+# my_tuple = (1, 2, 3, 4)
+# for item in my_tuple:
+#     print(item)
+
+my_list = [1, 2, 3]
+my_tuple = tuple(my_list)
+print(my_tuple)
